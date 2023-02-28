30bdfc5febeceb31fa032352d3cd7f48d453fd7c
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-02-28 22:16:01 +0300

    project done

--- email_sender/settings.py
+++ email_sender/settings.py
@@ -52,6 +52,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'email_sender.urls'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kiptootitus75@gmail.com'
+EMAIL_HOST_PASSWORD = 'uoibhxjmolpqrbpf'
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- send/views.py
+++ send/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+
 
 # Create your views here.
+def index(request):
+    send_mail('Hello Titus ', 
+    ' Hello there this is Titus, testing django send mail project, if you get so thumbs up', 
+    'kiptootitus75@gmail.com',
+    ['s.sanbf@gmail.com', 'jeptoomercy2001@gmail.com', 'tituskiptoo65@gmail.com'],
+    fail_silently=False)
+    return render(request, 'index.html')
